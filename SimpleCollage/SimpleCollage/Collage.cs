ad44d2341a57d810aa21eee0998de381e6283e35
Chance Hollis <chancehollis@gmail.com>
2017-10-11 15:15:09 -0600

    Working on making the collage with the collageimage and template

---
+++ SimpleCollage/SimpleCollage/Collage.cs
@@ -0,0 +1,60 @@
+﻿using SimpleCollage.Controllers;
+using SimpleCollage.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCollage
+{
+    public class Collage
+    {
+        public CollageImage[] CollageImages { get; set; }
+        public CollageTemplateImage Template { get; set; }
+        public CollageImage[,] CollageLayout { get; set; }
+
+        public Collage(string templateImageLocation, string imagesLocation)
+        {
+            ImageFileIO io = new ImageFileIO();
+
+            Image i = io.ImageFromFile(templateImageLocation);
+            CollageTemplateImage c = new CollageTemplateImage(i);
+            c.GenerateTemplateValues();
+
+            IEnumerable<string> fileNames = Directory.EnumerateFiles(imagesLocation);
+            Image[] images = new Image[fileNames.Count()];
+            
+            for (int j = 0; j < fileNames.Count(); ++j)
+            {
+                images[j] = io.ImageFromFile(fileNames.ElementAt(j));
+            }
+
+            CollageImages = new CollageImage[fileNames.Count()];
+            for (int j = 0; j < images.Count(); ++j)
+            {
+                CollageImage c = new CollageImage(images[j]);
+                CollageImages[j] = c;
+                Console.WriteLine(c.AvgRGB);
+            }
+        }
+
+        /// <summary>
+        /// This method will take the list of images and the template image and create a 2d array of CollageImages, the collage
+        /// </summary>
+        public void GenerateCollage()
+        {
+            CollageLayout = new CollageImage[Template.TemplateValues.GetLength(0), Template.TemplateValues.GetLength(1)];
+
+            for (int row = 0; row < Template.TemplateImage.Height; ++row)
+            {
+                for (int col = 0; col < Template.TemplateImage.Width; ++col)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ SimpleCollage/SimpleCollage/Models/CollageImage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCollage.Models
+{
+    public class CollageImage
+    {
+
+        // Properties
+        public Image CImage { get; set; }
+        public double AvgRGB { get; set; }
+
+        // constants 
+        private const double redWeight = 0.33;
+        private const double greenWeight = 0.33;
+        private const double BlueWeight = 0.33;
+
+        /// <summary>
+        /// Recieves an image, then calculates the average rgb for the image to create a collageImage object
+        /// </summary>
+        /// <param name="img"></param>
+        public CollageImage(Image img)
+        {
+            CImage = img;
+            getAvgRGB();
+        }
+
+        /// <summary>
+        /// Loops over the pixels in the image to get an average rgb across every pixel in the image
+        /// 
+        /// -- Could be optimized
+        /// learned from "stackoverflow.com/questions/1068373/how-to-calculate-the-average-rgb-color-values-of-a-bitmap" user: Loofer
+        /// </summary>
+        private void getAvgRGB()
+        {
+            double avgR = 0;
+            double avgG = 0;
+            double avgB = 0;
+            int pixelCount = 0;
+
+            if (CImage != null)
+            {
+                Bitmap bmp = new Bitmap(CImage);
+
+                for (int row = 0; row < bmp.Height; ++row)
+                {
+                    for (int col = 0; col < bmp.Width; ++col)
+                    {
+                        Color pixelColor = bmp.GetPixel(col, row);
+                        avgR += pixelColor.R;
+                        avgG += pixelColor.G;
+                        avgB += pixelColor.B;
+                        pixelCount++;
+                    }
+                }                
+            }
+
+            double r = avgR / pixelCount;
+            double g = avgG / pixelCount;
+            double b = avgB / pixelCount;
+
+            AvgRGB = (r * redWeight) + (g * greenWeight) + (b * BlueWeight);
+        }
+    }
+}
